4ce7875369d480dff66f3cf0f4f851e43122752b
Jeremy <jeremybaez919@gmail.com>
2026-05-19 11:29:12 -0700

    Credit card and fixed asset complete

--- src/main/java/com/pluralsight/CreditCard.java
+++ src/main/java/com/pluralsight/CreditCard.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
-public class CreditCard {
+public class CreditCard implements Valuable{
+    private String owner;
+    private double balance;
+    private int cardNumber;
+
+    public CreditCard(String owner, double balance, int cardNumber) {
+        this.owner = owner;
+        this.balance = balance;
+        this.cardNumber = cardNumber;
+    }
+    public int getCardNumber() {
+        return cardNumber;
+    }
+    public String getOwner(){
+        return owner;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void makePurchase(double amount){
+        balance += amount;
+    }
+    public void makePayment(double amount) {
+        balance -= amount;
+    }
+
+    // The value of a credit card is negative because it represents money owed.
+    @Override
+    public double getValue(){
+        return -balance;
+    }
 }
--- src/main/java/com/pluralsight/FixedAsset.java
+++ src/main/java/com/pluralsight/FixedAsset.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
-public class FixedAsset{
+public abstract class FixedAsset implements Valuable {
+
+    private String name;
+    private double value;
+
+    //constructor
+    public FixedAsset(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+    //getter
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getValue() {
+        return value;
+    }
+    //setter
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+
+
+
 }
